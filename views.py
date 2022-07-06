38c2728b45b1081236c472eb3dc8448d6764abee
Igor Sverchkov <knjaz1989@gmail.com>
2022-07-06 12:22:52 +0300

    mail fixed

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 import smtplib
 import os
+import time
 from email.message import EmailMessage
 from dotenv import load_dotenv
 from flask import render_template, request
@@ -16,15 +17,21 @@ def show_page():
 
 
 def send_mail():
-    mail = request.form['mail']
+    user_mail = request.form['mail'].lower()
     text = request.form['text']
-
-    s = smtplib.SMTP(host='smtp.mail.ru', port=25)
-    s.starttls()
-    s.ehlo()
-    s.login(LOGIN, PASSWORD)
-    s.sendmail(LOGIN, LOGIN, text)
-    s.quit()
+    address_list = [[LOGIN, text, "Новая заявка"], [user_mail, "Ваша заявка принята", "Заявка на перевод"]]
+    for item in address_list:
+        msg = EmailMessage()
+        msg.set_content(item[1])
+        msg['Subject'] = item[2]
+        msg['From'] = LOGIN
+        msg['To'] = item[0]
+        s = smtplib.SMTP(host='smtp.mail.ru', port=25)
+        s.starttls()
+        s.ehlo()
+        s.login(LOGIN, PASSWORD)
+        s.send_message(msg)
+        s.quit()
     return render_template("index.html")
 
 
