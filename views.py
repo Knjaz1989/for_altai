c7878889cd59505a5ed5051e9cbb8bf711bc3799
Igor Sverchkov <knjaz1989@gmail.com>
2022-07-12 11:58:00 +0300

    v4

--- views.py
+++ views.py
@@ -1,6 +1,5 @@
 import smtplib
 import os
-import time
 from email.message import EmailMessage
 from dotenv import load_dotenv
 from flask import render_template, request
@@ -8,31 +7,42 @@ from app import application
 
 load_dotenv()
 
-LOGIN = os.getenv("LOGIN")
-PASSWORD = os.getenv("PASSWORD")
-
 
 def show_page():
     return render_template("index.html")
 
 
+def send_to_user(from_, password, to, subject, content):
+    msg = EmailMessage()
+    msg.set_content(content)
+    msg['Subject'] = subject
+    msg['From'] = from_
+    msg['To'] = to
+    s = smtplib.SMTP(host='smtp.mail.ru', port=25)
+    s.starttls()
+    s.ehlo()
+    s.login(from_, password)
+    s.send_message(msg)
+    s.quit()
+
+
 def send_mail():
+    LOGIN = os.getenv("LOGIN")
+    PASSWORD = os.getenv("PASSWORD")
     user_mail = request.form['mail'].lower()
     text = request.form['text']
-    address_list = [[LOGIN, text, "Новая заявка"], [user_mail, "Ваша заявка принята", "Заявка на перевод"]]
+    address_list = [
+        [user_mail, "Заявка на перевод", "Ваша заявка принята"],
+        [LOGIN, "Новая заявка", text]
+    ]
+    data = None
     for item in address_list:
-        msg = EmailMessage()
-        msg.set_content(item[1])
-        msg['Subject'] = item[2]
-        msg['From'] = LOGIN
-        msg['To'] = item[0]
-        s = smtplib.SMTP(host='smtp.mail.ru', port=25)
-        s.starttls()
-        s.ehlo()
-        s.login(LOGIN, PASSWORD)
-        s.send_message(msg)
-        s.quit()
-    return render_template("index.html")
+        try:
+            send_to_user(LOGIN, PASSWORD, item[0], item[1], item[2])
+        except Exception as err:
+            data = "Неверный адрес электронной почты"
+            break
+    return render_template("index.html", data=data)
 
 
 application.add_url_rule('/', view_func=show_page)
